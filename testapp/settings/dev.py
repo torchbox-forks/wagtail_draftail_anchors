2ccfa7e0f465e82dd8f45f5370f411f6df183480
nickmoreton <nickmoreton@me.com>
2025-10-31 13:14:59 +0000

    Create a testapp

---
+++ testapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-pmpj-fmvq0l!wut=8^(qs!=nb!-lv7v6(f_y!%klpf!&eqn3q^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
